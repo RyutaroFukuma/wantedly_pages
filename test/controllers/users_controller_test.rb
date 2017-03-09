7f2073102cd39d9ab8760d3d045b7e3e006ae314
Ryutaro <ricken0203@gmail.com>
2017-03-09 11:21:26 +0900

    [add] StaticPagesコントローラーとhomeビューの作成

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,17 +1,9 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get users_index_url
-    assert_response :success
-  end
-
-  test "should get show" do
-    get users_show_url
-    assert_response :success
-  end
 
   test "should get new" do
+    get signup_path
     get users_new_url
     assert_response :success
   end
