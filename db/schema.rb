29ce3d364db8a9bd4cd0589fd46379f8dbdb14c3
Ryutaro <ricken0203@gmail.com>
2017-03-09 17:25:40 +0900

    [update] メールアドレスの一意性を検証するテストコードの記述

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170308160813) do
+ActiveRecord::Schema.define(version: 20170309081409) do
 
   create_table "skills", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 20170308160813) do
     t.string   "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
   end
 
   add_foreign_key "user_skills", "skills"
