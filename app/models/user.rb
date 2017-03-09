e6819c04151b56d4681d83627d5a63cd15a5bffb
Ryutaro <ricken0203@gmail.com>
2017-03-09 16:57:33 +0900

    [update] Usersの存在性を検証するテストコードを記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ApplicationRecord
   has_many :user_skills
+  validates :name, presence: true
+  validates :email, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'static_pages/home'
+
   get 'user', to: 'user#index'
   get 'user/signup', to:'user#new'
   get 'user/:id', to: 'user#show'
