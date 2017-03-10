3e2e27952ad17463395462d5c9037c638691b610
Ryutaro <ricken0203@gmail.com>
2017-03-11 00:55:48 +0900

    [update] ユーザー登録機能を実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,7 +15,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
    if @user.save
-
+     flash[:success] = "Welcome to the Wantedly"
+    redirect_to @user
    else
     render 'new'
    end
@@ -24,7 +25,7 @@ class UsersController < ApplicationController
   private
 
    def user_params
-     params.require(:user).parmit(:name, :email, :password,
+     params.require(:user).permit(:name, :email, :password,
                     :password_confirmation)
    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
 
   root 'static_pages#home'
 
-  get '/signup', to:'users#new'
+  get '/signup', to: 'users#new'
+  post '/signup', to: 'users#create'
 
   get 'user_skills/plus', to: 'user_skills#plus'
 
