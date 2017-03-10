ad02ec637697302cb8e191446e4b5995ee288360
Ryutaro <ricken0203@gmail.com>
2017-03-11 01:52:23 +0900

    [update] ログイン機能の実装

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,12 @@
 class SessionsController < ApplicationController
   def new
   end
-i
+
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      #ユーザーログイン後にユーザー情報のページにリダイレクトする
+      log_in user
+      redirect_to user
     else
       flash.now[:danger] = 'Invalid email/password combination'
     render 'new'
