a4cfb15fefe1e2f7d435291cfaa0df24b30a72e8
Ryutaro <ricken0203@gmail.com>
2017-03-11 01:17:17 +0900

    [Add]  Sessionsコントローラーの作成とルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  get 'sessions/new'
+
   root 'static_pages#home'
 
   get '/signup', to: 'users#new'
@@ -10,5 +12,11 @@ Rails.application.routes.draw do
   get 'skills', to: 'skills#index'
   post 'skills', to: 'skills#create'
   get 'skills/:id', to: 'skills#show'
+
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
+
+
   resources :users
 end
