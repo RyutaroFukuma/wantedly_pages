f3a8357a0c83a9e749c9f048a39a98880c098833
Ryutaro <ricken0203@gmail.com>
2017-03-10 11:47:10 +0900

    [update] +1 機能の修正

--- app/controllers/user_skills_controller.rb
+++ app/controllers/user_skills_controller.rb
@@ -5,6 +5,6 @@ class UserSkillsController < ApplicationController
     user_skill = UserSkill.find(id)
     user_skill.count += 1
     user_skill.save
-    redirect_to "/user/#{user_skill.user.id}"
+    redirect_to "/users/#{user_skill.user.id}"
   end
 end
