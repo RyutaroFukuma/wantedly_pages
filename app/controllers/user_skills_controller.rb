8b6a57820cbf36e2d192eff04b9b4866e9d87a46
Ryutaro <ricken0203@gmail.com>
2017-03-18 11:15:05 +0900

    [update] +1 ボタンをlink_toで実装

--- app/controllers/user_skills_controller.rb
+++ app/controllers/user_skills_controller.rb
@@ -1,12 +1,10 @@
 class UserSkillsController < ApplicationController
   def plus
-    user_id = params[:user_id]
     id = params[:user_skill_id]
     user_skill = UserSkill.find(id)
     user_skill.count += 1
     user_skill.save
-    redirect_to "/users/#{user_id}"
-
+    redirect_to "/users/#{user_skill.user.id}"
     #redirect_to "/users/#{user_skill.user.id}"
   end
 end
