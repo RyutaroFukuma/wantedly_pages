398e00ce7c67c8c21d3a22ca99f0a7268bc16bcd
Ryutaro <ricken0203@gmail.com>
2017-03-18 11:10:38 +0900

    [update] seeds.rbの更新

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+names = %w(tom jay helen mook fox pd jon jonny)
+
+names.each do |name|
+  User.create(
+      name: name,
+      email: "#{name}@hogehoge.com",
+      password: 'password'
+  )
+end
+
